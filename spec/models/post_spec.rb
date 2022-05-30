b563b62a53ae54f6d209112293ae0c435a014865
itsuto <p-haya.p.p.8810@docomo.ne.jp>
2022-05-30 18:52:28 +0900

    投稿機能テストコード

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @post = FactoryBot.build(:post)
+  end
+
+  describe '練習メニュー投稿機能' do
+    context '投稿できる時' do
+      it 'titleとcontentとcategory_idとgenre_idが存在すれば投稿できる' do
+        expect(@post).to be_valid
+      end
+    end
+    context '投稿できない時' do
+      it 'titleが空では投稿できない' do
+        @post.title = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Title can't be blank")
+      end
+      it 'contentが空では投稿できない' do
+        @post.content = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Content can't be blank")
+      end
+      it 'category_idがからでは投稿できない' do
+        @post.category_id = 1
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'genre_idが空では投稿できない' do
+        @post.genre_id = 1
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Genre can't be blank")
+      end
+      it 'userが紐づいていないと投稿できない' do
+        @post.user = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include('User must exist')
+      end
+    end
+  end
 end
