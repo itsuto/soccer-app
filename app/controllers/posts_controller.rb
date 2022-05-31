09dfd722944c89a1032d1004c8014b07b79c2091
itsuto <p-haya.p.p.8810@docomo.ne.jp>
2022-05-31 17:15:01 +0900

    詳細表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,17 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+    @post = Post.find(params[:id])
+
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
   private
 
   def post_params
