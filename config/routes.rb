c03100b09a43a46c1593b14bc4cb3ebe395573a9
itsuto <p-haya.p.p.8810@docomo.ne.jp>
2022-06-03 17:36:37 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
   resources :users, only: [:show]
+  resources :comments, only: :create
   resources :posts do
     collection do
       get 'search'
