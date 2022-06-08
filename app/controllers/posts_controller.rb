79d6c363181e7775f7c0991775206c4c30a80d3c
itsuto <p-haya.p.p.8810@docomo.ne.jp>
2022-06-09 00:16:21 +0900

    練習メニュー動画投稿

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -48,7 +48,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, :category_id, :genre_id).merge(user_id: current_user.id)
+    params.require(:post).permit(:title, :content, :category_id, :genre_id, :video).merge(user_id: current_user.id)
   end
 
   def set_params
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ApplicationRecord
   belongs_to :category
   belongs_to :user
   has_many :comments
+  has_one_attached :video
+
 
   validates :title, :content, presence: true
   validates :genre_id, numericality: { other_than: 1, message: "を選択してください" }
